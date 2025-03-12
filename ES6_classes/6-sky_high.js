9946129ca126da5e14b6ed26fcc05558a0ae5120
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-03-12 12:34:34 -0500

    task6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,26 @@
+import Building from './5-building';
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+    // properties
+    this._floors = floors;
+  }
+
+  // getters
+  get floors() {
+    return this._floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  // evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
