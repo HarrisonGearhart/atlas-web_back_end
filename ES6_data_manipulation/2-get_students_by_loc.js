e6ecda06e7a85e4f994b9e73f6a41bc0b0df58b4
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-03-12 13:10:27 -0500

    task2lint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,7 @@
 export default function getStudentsByLocation(students, city) {
-  // validate array 
+  // validate array
   if (Array.isArray(students)) {
     // filter function
-    return students.filter((person) => person.location == city);
+    return students.filter((person) => person.location === city);
   }
 }
