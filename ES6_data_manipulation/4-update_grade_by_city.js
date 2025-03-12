ae837c84e9d1229ef0980db41e1dfed684b49984
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-03-12 13:25:22 -0500

    task4lint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.filter((student) => student.location === city).map((student) => {
     const grade = newGrades.find((gr4de) => gr4de.studentId === student.id);
     return {
-      ... student, grade: grade ? grade.grade : 'N/A',
+      ...student, grade: grade ? grade.grade : 'N/A',
     };
   });
 }
