1c3eb0a68f4a0b20b665610ec77e728d73f6b3bf
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-03-12 10:51:29 -0500

    line6 and 7 typo

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
         const task2 = true;
   
     if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
+        const taskUp = true;
+        const task2Up = false;
         return [taskUp, task2Up];
     }
   
