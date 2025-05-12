f7a56bfb910dba9b6b87860948f83a745bf7d617
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-05-11 21:46:47 -0500

    task2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor which takes a float n
+as argument and returns the floor of the float
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    returns the floor of n (int)
+    import math library/find floor
+    """
+    return (math.floor(n))
