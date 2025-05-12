b06f0d22bcb2ed1736857d4f35c09d58c4e6e13b
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-05-11 22:07:00 -0500

    task1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+run multiple asynchronous coroutines concurrently
+"""
+import typing
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """
+    args - n int (times to run wait_random)
+    max_delay (time of delay in seconds)
+    returns - list[float] (listof all delays)
+    """
+    delays: typing.List[float] = []
+    for i in range(n):
+        delays.append(await wait_random(max_delay))
+
+    return sorted(delays)
+    
