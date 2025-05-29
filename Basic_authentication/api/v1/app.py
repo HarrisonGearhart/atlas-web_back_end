50ff5c51f86482028972838efb1b9812edee061f
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-05-28 22:07:18 -0500

    app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -3,59 +3,48 @@
 Route module for the API
 """
 from os import getenv
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-try:
-    import api.v1.views.app_views as app_views  # Sometimes only this works
-except ModuleNotFoundError:
-    from api.v1.views import app_views  # Other times only this works
-try:
-    import api.v1.auth.auth.Auth as Auth  # Sometimes only this works
-except ModuleNotFoundError:
-    from api.v1.auth.auth import Auth  # Other times only this works
+import os
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# variable auth is None
 auth = None
-auth_type = getenv('AUTH_TYPE')
-
-if auth_type == "auth":
-    auth = Auth()
-elif auth_type == "basic_auth":
+# if AUTH_TYPE is basic_auth
+# then import basiic_auth
+if getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+# else if AUTH_TYPE is auth
+# then local variable auth = Auth()
+elif getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
-@app.errorhandler(401)
-def unauth(error) -> str:
-    """ Unauthorized error handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ The Forbidden Error Handler that handles the "403: Forbidden" error
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
+# before request performs actions each time a request is made
 @app.before_request
-def before_request_auth():
+def before_request_func():
     """
-    Filter each request before it's handled
+    run before every request
     """
-    excl = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if auth is None or not auth.require_auth(request.path, excl):
+    if auth is None:
+        return
+
+    # list of paths
+    path_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+
+    # auth require_auth of request.path and path_list
+    if not auth.require_auth(request.path, path_list):
         return
 
     if auth.authorization_header(request) is None:
@@ -65,8 +54,35 @@ def before_request_auth():
         abort(403)
 
 
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+# 401 error
+@app.errorhandler(401)
+def error_unauthorized(error):
+    """
+    handler for 401 error
+    """
+    # jasonify the error messsage
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+# 403 error
+@app.errorhandler(403)
+def error_forbidden(error):
+    """
+    handler for 403 error
+    """
+    # jasonify the error messsage
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
+    port = getenv("API_PORT", "5005")
     app.run(host=host, port=port)
-
+    
