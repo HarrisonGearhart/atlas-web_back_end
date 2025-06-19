a5330440c565353b7b7206004c3e5626a8c25d2c
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-06-18 22:49:25 -0500

    tasks 7-9

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,13 @@
+-- Creates a stored procedure ComputeAverageScoreForUser that computes
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+SELECT AVG(score) INTO avg_score
+FROM corrections
+WHERE corrections.user_id = user_id;
+UPDATE users
+SET average_score = avg_score
+WHERE id = user_id;
+END; //
+DELIMITER ;
