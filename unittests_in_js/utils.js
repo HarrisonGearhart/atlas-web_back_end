eb5ea2f51ba37e7bc3d2184ea271f73f297c8de2
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-07-02 22:35:06 -0500

    task3

---
+++ unittests_in_js/utils.js
@@ -0,0 +1,23 @@
+// utils.js
+const Utils = {
+    calculateNumber: function (type, a, b) {
+      const aRounded = Math.round(a);
+      const bRounded = Math.round(b);
+  
+      if (type === 'SUM') {
+        return aRounded + bRounded;
+      } else if (type === 'SUBTRACT') {
+        return aRounded - bRounded;
+      } else if (type === 'DIVIDE') {
+        if (bRounded === 0) {
+          return 'Error';
+        }
+        return aRounded / bRounded;
+      }
+  
+      throw new Error('Invalid operation type');
+    }
+  };
+  
+  module.exports = Utils;
+  
